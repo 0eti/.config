4e8879ec33d38390f27db907ec6a6dbe2e7dec12
0eti <samuelzhang012@gmail.com>
2022-09-14 14:29:45 -0700

    nvim: nvim-tree updated

--- nvim/lua/user/plugins/layout/nvim-tree.lua
+++ nvim/lua/user/plugins/layout/nvim-tree.lua
@@ -1,10 +1,22 @@
+
 require("nvim-tree").setup({
   view = {
     mappings = {
       list = {
         { key = { "l", "<Tab>" }, action = "preview" },
-        { key = { "h", "<BS>" }, action = "close_node" }
+        { key = { "h", "<BS>" }, action = "close_node" },
       },
     },
   },
+  renderer = {
+    group_empty = true,
+    highlight_git = true,
+    highlight_opened_files = "all",
+    indent_markers = { enable = true },
+  },
+  filters = {
+    custom = { "^.git$" },
+    exclude = {},
+  },
 })
+
